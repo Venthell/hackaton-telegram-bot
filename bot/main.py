6c531e1156c3eb229697ac2601cde1f9e3ab3d6d
Venthell <venthell6@gmail.com>
2023-05-08 18:49:29 +0300

    now it becomes echo

--- bot/main.py
+++ bot/main.py
@@ -1,27 +1,34 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# token
+API_TOKEN: str = 'BOT TOKEN HERE'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
 
 
-API_URL: str = 'https://api.telegram.org/bot'
-BOT_TOKEN: str = '6094985598:AAE50dKeqOORC3nEuCxO2QHG4Z4UklRaunw'
-offset: int = -2
-updates: dict
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
-def do_something() -> None:
-    print('Был апдейт')
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
 
 
-while True: 
-    start_time = time.time()
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
 
-    if updates['result']:
-        print(updates['result'])
-        for result in updates['result']:
-            offset = result['update_id']
-            do_something()
 
-    time.sleep(3)
-    end_time = time.time()
-    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
+if __name__ == '__main__':
+    dp.run_polling(bot)
