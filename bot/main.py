0c8964704152d9b3349fdade68ba517b92d3cd29
Venthell <venthell6@gmail.com>
2023-05-08 18:33:51 +0300

    k, let it be foxes

--- bot/main.py
+++ bot/main.py
@@ -1,32 +1,31 @@
 import requests
 import time
 
-
 API_URL: str = 'https://api.telegram.org/bot'
-API_DOGS_URL: str = 'https://random.dog/woof.json'
-BOT_TOKEN: str = '5424991242:AAGwomxQz1p46bRi_2m3V7kvJlt5RjK9xr0'
-ERROR_TEXT: str = 'Здесь должна была быть картинка с собачкой :('
+API_FOX_URL: str = 'https://randomfox.ca/floof/'
+BOT_TOKEN: str = '6094985598:AAE50dKeqOORC3nEuCxO2QHG4Z4UklRaunw'
+ERROR_TEXT: str = 'Здесь должна была быть картинка :('
 
 offset: int = -2
 counter: int = 0
-dog_response: requests.Response
-dog_link: str
-
+fox_response: requests.Response
+fox_link: str
+ 
 
 while counter < 100:
     print('attempt =', counter)
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            fox_response = requests.get(API_FOX_URL)
+            if fox_response.status_code == 200:
+                fox_link = fox_response.json()['image']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={fox_link}')
             else:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+ 
 
     time.sleep(1)
     counter += 1
