33e448d03809c26f2a36a3a87751942f756c9a2b
Venthell <venthell6@gmail.com>
2023-05-08 18:38:25 +0300

    long polling test

--- bot/main.py
+++ bot/main.py
@@ -1,31 +1,26 @@
 import requests
 import time
 
+
 API_URL: str = 'https://api.telegram.org/bot'
-API_FOX_URL: str = 'https://randomfox.ca/floof/'
-BOT_TOKEN: str = '6094985598:AAE50dKeqOORC3nEuCxO2QHG4Z4UklRaunw'
-ERROR_TEXT: str = 'Здесь должна была быть картинка :('
-
+BOT_TOKEN: str = '5424991242:AAGwomxQz1p46bRi_2m3V7kvJlt5RjK9xr0'
 offset: int = -2
-counter: int = 0
-fox_response: requests.Response
-fox_link: str
- 
+updates: dict
 
-while counter < 100:
-    print('attempt =', counter)
+
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True: 
+    start_time = time.time()
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            fox_response = requests.get(API_FOX_URL)
-            if fox_response.status_code == 200:
-                fox_link = fox_response.json()['image']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={fox_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
- 
+            do_something()
 
-    time.sleep(1)
-    counter += 1
+    time.sleep(3)
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
