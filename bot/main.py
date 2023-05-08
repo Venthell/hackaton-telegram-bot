3f9e4b629551e32b9e97616a0c870773d38c00aa
Venthell <venthell6@gmail.com>
2023-05-08 18:28:51 +0300

    now it sends a cat pictures :)

--- bot/main.py
+++ bot/main.py
@@ -1,32 +1,32 @@
 import requests
 import time
-from bot.filters import register_all_filters
-from bot.misc import TgKeys
-from bot.handlers import register_all_handlers
-from bot.database.models import register_models
 
 
 API_URL: str = 'https://api.telegram.org/bot'
-BOT_TOKEN: str = '6094985598:AAE50dKeqOORC3nEuCxO2QHG4Z4UklRaunw'
-TEXT: str = 'AMAZING'
-MAX_COUNTER: int = 100
+API_CATS_URL: str = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN: str = '5424991242:AAGwomxQz1p46bRi_2m3V7kvJlt5RjK9xr0'
+ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
 
 offset: int = -2
 counter: int = 0
-chat_id: int
+cat_response: requests.Response
+cat_link: str
 
 
-while counter < MAX_COUNTER:
-
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-
+while counter < 100:
+    print('attempt =', counter)
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1
